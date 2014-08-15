2885f2c518bc24deb744a6c361d0a5bc1645c8c5
Dan Simonson <dan.simonson@infochimps.com>
2014-08-15 15:11:45 -0500

    srpgo method indentation and code clean-up

--- lib/thunder/subcommand/sherpa.rb
+++ lib/thunder/subcommand/sherpa.rb
@@ -32,133 +32,126 @@ module Thunder
       no_commands do
         def srpgo(command, args)
           args = [command] + args
-          #stack_name = args[0]
-          #template_filename = args[1]
-          #parameters = args[2..-1]
 
-          #pardon my indentation
-          #I literally copied srpgo right in here, and
-          #I don't wanna stretch my window
+          # This script aids in launching a builder script by
+          #  1) compiling all of the client and foundation.rb templates to json
+          #  2) uploading template jsons to unique-named folder in an s3 bucket
+          #  3) launching thunder to create or update a named builder stack
 
-# This script aids in launching a builder script by
-#  1) compiling all of the client and foundation.rb templates into json
-#  2) uploading template jsons to a unique-named folder in an s3 bucket
-#  3) launching thunder to create or update a named builder stack
+          # Hard Coded Configuration
 
-# Hard Coded Configuration
+          # "templates.platform.infochimps" is owned by the "infochimps"
+          # account, but that it allows write access by the
+          # "srp1-infochimps" account into the /dev folder. Everything in this
+          # bucket is currently world readable, so do not put anything secret
+          # into a template!
 
-# "templates.platform.infochimps" is owned by the "infochimps"
-# account, but that it allows write access by the
-# "srp1-infochimps" account into the /dev folder. Everything in this
-# bucket is currently world readable, so do not put anything secret
-# into a template!
+          bucket_name = "templates.platform.infochimps"
 
-bucket_name = "templates.platform.infochimps"
+          # Also, it is configured to autodelete content from the "/dev"
+          # directory one day after its creation date. Other folders may hav
+          # similar rules in the future.
 
-# Also, it is configured to autodelete content from the "/dev"
-# directory one day after its creation date. Other folders may hav
-# similar rules in the future.
+          bucket_base_dir = "dev"
 
-bucket_base_dir = "dev"
+          # API access to s3 buckets can be a pain. For this one, we HAVE to go
+          # through us-east-1. Even if you are going to be creating stacks in a
+          # different region do not change this value unless you have set up 
+          # your own bucket and you know what you are doing.
+          bucket_region = "us-east-1"
 
-# API access to s3 buckets can be a pain. For this one, we HAVE to go through
-# us-east-1. Even if you are going to be creating stacks in a different region
-# do not change this value unless you have set up your own bucket and you
-# know what you are doing.
-bucket_region = "us-east-1"
+          # Where to put generated files
+          temp_dir = ".upload"
 
-# Where to put generated files
-temp_dir = ".upload"
+          # Name of the paramteter to use to tell the builder stack where to 
+          # find its client jsons
+          template_url_parameter = "TemplateUrlBase"
 
-# Name of the paramteter to use to tell the builder stack where to find its client jsons
-template_url_parameter = "TemplateUrlBase"
+          # launch-kickoff home directory
+          # THIS IS GONNA BE A TRICKY FIX -- add to .thunder config?
+          # Travis suggested adding this as a "find the launch-kickoff dir"
+          # utility function somewhere. A good idea.
+          kickoff_home = ENV["PWD"]
+          err = "sherpa must be run from launch-kickoff."
+          raise Exception.new(err) unless /^.*launch-kickoff$/.match(kickoff_home)
 
-# launch-kickoff home directory
-# THIS IS GONNA BE A TRICKY FIX -- add to .thunder config?
-# Travis suggested adding this as a "find the launch-kickoff dir"
-# utility function somewhere. A good idea.
-kickoff_home = "/Users/dansimonson/Desktop/kickoff/launch-kickoff" #File.expand_path('../..', __FILE__)
+          # If any generation parameters are supplied, they must be at the end 
+          # of the thunder command line. These will be passed to cfndsl when we 
+          # build the clients. Currently these must be yaml
 
-kickoff_home = ENV["PWD"]
-err = "sherpa must be run from launch-kickoff."
-raise Exception.new(err) unless /^.*launch-kickoff$/.match(kickoff_home)
+          #Divide the parameters into generation and non-generation, 
+          # pivoting around -g
 
-# If any generation parameters are supplied, they must be at the end of the thunder
-# command line. These will be passed to cfndsl when we build the clients. Currently these
-# must be yaml
+          generation_parameters = []
+          non_generation_parameters = args
+          generation_parameters = args[ args.index("-g")+1..-1 ] if args.index("-g")
+          non_generation_parameters = args[ 0...args.index("-g") ] if args.index("-g")
 
-#Divide the parameters into generation and non-generation, pivoting around -g
+          #adds -y before each of generation_parameters
+          cfndsl_gen_parms = generation_parameters.inject([]) { |a,i| a.push("-y",i) }
 
-generation_parameters = []
-non_generation_parameters = args
-generation_parameters = args[ args.index("-g")+1..-1 ] if args.index("-g")
-non_generation_parameters = args[ 0...args.index("-g") ] if args.index("-g")
+          # Presently, this script is only designed to pass stuff on to thunder to do the actual
+          # launch of a builder-style script.
 
-#adds -y before each of generation_parameters
-cfndsl_gen_parms = generation_parameters.inject([]) { |a,i| a.push("-y",i) }
+          unless system("aws --version > /dev/null") then
+            puts "This script requires the aws-cli tools to be installed."
+            exit(-1)
+          end
 
-# Presently, this script is only designed to pass stuff on to thunder to do the actual
-# launch of a builder-style script.
+          # Generate unique filename
+          user = Etc::getpwuid.name
+          time = Time.now.strftime('%Y%m%dT%H%M%S%z')
+          uuid = SecureRandom.uuid
 
-unless system("aws --version > /dev/null") then
-  puts "This script requires the aws-cli tools to be installed."
-  exit(-1)
-end
+          uniq=[user,time,uuid].join("-")
 
-# Generate unique filename
-user = Etc::getpwuid.name
-time = Time.now.strftime('%Y%m%dT%H%M%S%z')
-uuid = SecureRandom.uuid
-
-uniq=[user,time,uuid].join("-")
-
-updir = FileUtils.mkdir_p( File.join(temp_dir,uniq) )[0]
-cldir = FileUtils.mkdir_p( File.join(updir,"clients" ))[0]
-fddir = FileUtils.mkdir_p( File.join(updir,"foundation"))[0]
+          updir = FileUtils.mkdir_p( File.join(temp_dir,uniq) )[0]
+          cldir = FileUtils.mkdir_p( File.join(updir,"clients" ))[0]
+          fddir = FileUtils.mkdir_p( File.join(updir,"foundation"))[0]
 
 
-#run cfndsl for rb files and pipe each to the appropriate JSON file.
+          #run cfndsl for rb files and pipe each to the appropriate JSON file.
 
-Dir.glob("#{kickoff_home}/clients/*.rb") do |infile|
-  outfile = File.join(cldir,File.basename(infile,".rb")+".json")
-  ok = system "cfndsl #{infile} #{cfndsl_gen_parms.join ' '} > #{outfile}"
-  if !ok then
-    STDERR.puts "cfndsl failed on #{infile}. stopping."
-    exit(1)
-  end
-end
+          Dir.glob("#{kickoff_home}/clients/*.rb") do |infile|
+            outfile = File.join(cldir,File.basename(infile,".rb")+".json")
+            ok = system "cfndsl #{infile} #{cfndsl_gen_parms.join ' '} > #{outfile}"
+            if !ok then
+              STDERR.puts "cfndsl failed on #{infile}. stopping."
+              exit(1)
+            end
+          end
 
-Dir.glob("#{kickoff_home}/foundation/*foundation*.rb") do |infile|
-  outfile = File.join(fddir,File.basename(infile,".rb")+".json")
-  ok = system "cfndsl #{infile} #{cfndsl_gen_parms.join ' '} > #{outfile}"
-  if !ok then
-    STDERR.puts "cfndsl failed on #{infile}. stopping."
-    exit(1)
-  end
-end
+          Dir.glob("#{kickoff_home}/foundation/*foundation*.rb") do |infile|
+            outfile = File.join(fddir,File.basename(infile,".rb")+".json")
+            ok = system "cfndsl #{infile} #{cfndsl_gen_parms.join ' '} > #{outfile}"
+            if !ok then
+              STDERR.puts "cfndsl failed on #{infile}. stopping."
+              exit(1)
+            end
+          end
 
-#what's being synced here? the cfndsl outputs.
-#TODO: use the aws-sdk to to do this?
-system "aws --region=#{bucket_region} s3 sync  #{updir} s3://#{bucket_name}/#{bucket_base_dir}/#{uniq}/  --acl=public-read"
+          #what's being synced here? the cfndsl outputs.
+          #TODO: use the aws-sdk to to do this?
+          system "aws --region=#{bucket_region} s3 sync  #{updir} s3://#{bucket_name}/#{bucket_base_dir}/#{uniq}/  --acl=public-read"
 
-#generate a new parameters file that indicates the location of the template_url
-parameters = { template_url_parameter => "https://s3.amazonaws.com/#{bucket_name}/#{bucket_base_dir}/#{uniq}" }
-params_file_name = File.join(updir,"#{uniq}_parameters.json")
-File.open(params_file_name,"w") do |f|
-  f.write(parameters.to_json)
-end
+          #generate a new parameters file that indicates the location of the template_url
+          parameters = { template_url_parameter => "https://s3.amazonaws.com/#{bucket_name}/#{bucket_base_dir}/#{uniq}" }
+          params_file_name = File.join(updir,"#{uniq}_parameters.json")
+          File.open(params_file_name,"w") do |f|
+            f.write(parameters.to_json)
+          end
 
 
-#assemble parameters filenames for thunder
-thunder_parameters = non_generation_parameters
-thunder_parameters.push params_file_name
-if generation_parameters.length > 0 then
-  thunder_parameters.push("-g")
-  thunder_parameters += generation_parameters
-end
+          #assemble parameters filenames for thunder
+          thunder_parameters = non_generation_parameters
+          thunder_parameters.push params_file_name
+          if generation_parameters.length > 0 then
+            thunder_parameters.push("-g")
+            thunder_parameters += generation_parameters
+          end
 
-puts "thunder stack " + thunder_parameters.join(" ")
-system("thunder stack " + thunder_parameters.join(" "))
+          puts "thunder stack " + thunder_parameters.join(" ")
+          system("thunder stack " + thunder_parameters.join(" "))
 
 
       end
