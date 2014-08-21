9ef4269fbe0d00ded5a2e8aa718521285c1a5538
Travis Dempsey <dempsey.travis@gmail.com>
2014-08-21 11:14:45 -0500

    run coverage if invoked

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,15 @@
-require 'thunder'
-
 require 'aruba'
 require 'aruba/api'
+
+if ENV['THUNDER_COV']
+  require 'simplecov'
+  SimpleCov.start do
+    add_group 'Specs',   'spec/'
+    add_group 'Library', 'lib/'
+  end
+end
+
+require 'thunder'
 
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each{ |f| require f }
 
