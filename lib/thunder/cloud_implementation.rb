4a7b4b8da75fafcde98033c9626161a6875fc8e4
Travis Dempsey <dempsey.travis@gmail.com>
2015-03-27 19:53:42 -0500

    simple syntax fixes

--- lib/thunder/cloud_implementation.rb
+++ lib/thunder/cloud_implementation.rb
@@ -22,18 +22,17 @@ module Thunder
     # HASHLOADING #
 
     def supported_format(extension, parsers)
-      return parsers.has_key?(extension)
+      parsers.key? extension
     end
 
     # Get a string that we can create a stack with from a parsed template
     def get_template_text(template)
-      if template.has_key?('_thunder_url')
+      if template.key? '_thunder_url'
         return template['_thunder_url']
       else
         return template.to_json
       end
     end
-
 
     # This loads a file as directed by parsers and returns the result
     def hashload(filename, parsers)
